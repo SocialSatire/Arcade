227ca86e0539ae2d0888d660e9dfba8257fa2333
Mike <cataclysmscepter@gmail.com>
2014-09-22 21:45:15 -0400

    Put some initialization stuff

--- games/Kevcycles.py
+++ games/Kevcycles.py
@@ -1,5 +1,18 @@
 import pygame
-pygame.init()
 
 # We need some more code here somebody get on that.
 #Thanks for the good start, Mike. I would never have figured out how to import the libraries.
+
+class Game:
+	def __init__(self, width=640,height=480):
+        pygame.init()
+        self.width = width
+        self.height = height
+        self.screen = pygame.display.set_mode(self.width, self.height)
+	
+	def play(self):
+		print			# We should be able to put the game code here
+
+if __name__ == "__main__":
+    GameWindow = Game()
+    GameWindow.play()
