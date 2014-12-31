f5d4081a1d2c89f95e763b97ffbdf5d603104829
Mike <cataclysmscepter@gmail.com>
2014-12-31 10:40:51 -0500

    Fixed a few bugs and stuff
    now it actually works I tested it.

--- java/openMenu.bat
+++ java/openMenu.bat
@@ -1,3 +1,7 @@
+@echo off
+cls
 javac -cp .;jars/slick.jar;jars/lwjgl.jar;games ArcadeMenu.java
+echo Compiling Complete
+pause
 java -Djava.library.path=lwjglbin -cp .;jars/slick.jar;jars/lwjgl.jar;games ArcadeMenu
 pause
